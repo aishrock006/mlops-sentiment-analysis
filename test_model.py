7364dc7cd8bf07ebd053d9a437ab3685b9f8eb14
Ashish kumar <aishrock006@gmail.com>
2024-08-07 18:28:07 +0530

    modified test model.py file

--- test_model.py
+++ test_model.py
@@ -4,6 +4,7 @@ from model import train_model
 def test_accuracy():
     accuracy = train_model()
     assert accuracy > 0.7, f"Accuracy is too low: {accuracy}"
+    print(f"Test passed with accuracy: {accuracy}")
 
 if __name__ == "__main__":
     test_accuracy()
